179252f39bb97cf5060c111afb8660357649de82
tranhoangduong1994 <tranhoangduong1994@gmail.com>
2019-02-18 15:51:41 +0700

    configurated eslint & integrated babel support

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component<Props> {
   render() {
     return (
       <View>
-        <Text testID="welcome">{'Welcome'}</Text>
+        <Text testID="welcome">Welcome</Text>
       </View>
     );
   }
